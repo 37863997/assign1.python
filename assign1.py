fa0f2e0f12ad147a9e2a7745ddbe0c216ce4da8e
Jesse Mugo <145917688+37863997@users.noreply.github.com>
2024-07-02 13:26:32 -0700

    Create assign1.py

---
+++ assign1.py
@@ -0,0 +1,10 @@
+# capture two numbers from the user
+num1=int(input("enter num1"))
+num2=int(input("enter num2"))
+# compute addition,subtraction,multiplication and division
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+#print the results of each operation
+print(f"addition is {addition} \n subtraction is {subtraction}\n multiplication is {multiplication}\n division is {division}")
